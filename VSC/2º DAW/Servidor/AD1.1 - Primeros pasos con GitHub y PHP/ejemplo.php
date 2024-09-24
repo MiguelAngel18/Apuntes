e2fd2cdaa8664e31f476608b25e8b37062c5bf06
MiguelAngel18 <migue.angel.c2005@gmail.com>
2024-09-24 13:28:45 +0100

    Clase de PHP

---
+++ VSC/2º DAW/Servidor/AD1.1 - Primeros pasos con GitHub y PHP/ejemplo.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+<?php 
+
+$a = 1;
+$b = 2;
+
+function test()
+{
+    global $a;
+    echo "Valor de a:" .$a, "<br>";
+}
+
+echo "<h1>Hola Mundo</h1>;"
+
+?>
+
+
+</body>
+</html>
