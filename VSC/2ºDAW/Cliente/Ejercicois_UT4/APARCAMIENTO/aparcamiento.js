84c9276e09aa38eb5a5a938c68fd8a4a204158fb
MiguelAngel18 <migue.angel.c2005@gmail.com>
2024-11-17 23:50:54 +0000

    .

---
+++ VSC/2ºDAW/Cliente/Ejercicois_UT4/APARCAMIENTO/aparcamiento.js
@@ -0,0 +1,10 @@
+let hoy = document.getElementById("fecha_entrada").value;
+let hora1 = document.getElementById("hora_entrada").value;
+
+function visualizar()
+{
+
+    let fecha2 = document.getElementById("fecha_salida").value;
+    let hora2 = document.getElementById("hora_salida").value;
+    let precio = 0.0;
+}
