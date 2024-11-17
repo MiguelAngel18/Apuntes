fe0c78beaf357cd2f79f7f1fcc8cf443946730a7
MiguelAngel18 <migue.angel.c2005@gmail.com>
2024-11-17 03:26:48 +0000

    .

--- VSC/2ºDAW/Cliente/Ejercicois_UT4/ahorcado.js
+++ VSC/2ºDAW/Cliente/Ejercicois_UT4/ahorcado.js
@@ -1,24 +1,63 @@
-function validar()
-{
-    let resultado = prompt("Introduce una palabra clave:");
+const password_1 = prompt("Usuario 1. Introduce una palabra clave:").toUpperCase();
+// const password_2 = prompt("Usuario 2. Introduce una palabra clave:").toUpperCase();
 
+let resultado_1 = password_1.replace(/./g, "_ ");
+document.getElementById('result_1').innerHTML = resultado_1;
+let fallos_1 = 0;
+let encontrada = false;
+
+// let resultado_2 = password_2.replace(/./g, "_ ");
+// document.getElementById('result_2').innerHTML = resultado_2;
+// let fallos_2 = 0;
+
+function validar_1()
+{
     try
     {
-        if(!isNaN(resultado))
+        let letra = document.getElementById("letra_1").value.toUpperCase();
+        if(letra==='')
         {
-            alert("El número introducido no es válido.");
+            return;
         }
 
-        if(resultado.length <= 1)
+        resultado_1 = password_1.replace(/./g, "_ ").split(" ");
+
+        for(let i=0; i<password_1.length; i++)
         {
-            alert("No has introducido nada.");
+            if(password_1[i] === letra)
+            {
+                resultado_1[i] = letra;
+                alert("Letra encontrada: " + letra);
+                encontrada = true;
+            }
         }
 
+        if(encontrada === false)
+        {
+            alert("Letra no encontrada");
+
+            switch(fallos_1)
+            {
+                case 1: document.getElementById('ahorcado').src = 'img/6.png'; break;
+                case 2: document.getElementById('ahorcado').src = 'img/5.png'; break;
+                case 3: document.getElementById('ahorcado').src = 'img/4.png'; break;
+                case 4: document.getElementById('ahorcado').src = 'img/3.png'; break;
+                case 5: document.getElementById('ahorcado').src = 'img/2.png'; break;
+                case 6: document.getElementById('ahorcado').src = 'img/1.png'; alert("Usuario 1 pierde"); break;
+                default: alert("No hay más intentos"); break;
+            }
+        }
         
+        alert(resultado_1);
+        document.getElementById('result_1').innerHTML = resultado_1;
 
+        if(!resultado_1.includes("_"))
+        {
+            alert("Usuario 1 gana.");
+        }
     }
     catch(err)
     {
-        document.getElementById("demo") = err.name;
+        alert("ERROR");
     }
 }
