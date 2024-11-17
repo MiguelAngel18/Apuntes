a78cecaae77f245f999796d9ce3c0a896d014ffe
MiguelAngel18 <migue.angel.c2005@gmail.com>
2024-11-17 18:06:00 +0000

    .

--- VSC/2ºDAW/Cliente/Ejercicois_UT4/ahorcado.js
+++ VSC/2ºDAW/Cliente/Ejercicois_UT4/ahorcado.js
@@ -1,43 +1,45 @@
-const password_1 = prompt("Usuario 1. Introduce una palabra clave:").toUpperCase();
-// const password_2 = prompt("Usuario 2. Introduce una palabra clave:").toUpperCase();
+const password_1 = prompt("Usuario 1. Introduce una palabra clave:").toUpperCase(); // Introduce la palabra clave del usuario 2
+const password_2 = prompt("Usuario 2. Introduce una palabra clave:").toUpperCase(); // Introduce la palabra clave del usuario 1
 
-let resultado_1 = password_1.replace(/./g, "_ ");
-document.getElementById('result_1').innerHTML = resultado_1;
-let fallos_1 = 0;
-let encontrada = false;
+let resultado_1 = password_2.replace(/./g, "_ ");  // Se emcripta la palabra clave del usuario 2
+let resultado_2 = password_1.replace(/./g, "_ ");  // Se emcripta la palabra clave del usuario 1
 
-// let resultado_2 = password_2.replace(/./g, "_ ");
-// document.getElementById('result_2').innerHTML = resultado_2;
-// let fallos_2 = 0;
+document.getElementById('result_1').innerHTML = resultado_1; // Se muestra la palabra clave encriptada del usuario 2
+document.getElementById('result_2').innerHTML = resultado_2; // Se muestra la palabra clave encriptada del usuario 1
+
+let fallos_1 = 0; let fallos_2 = 0; // Contador de fallos de los usuarios 1 y 2
+
+let encontrada_1 = false; let encontrada_2 = false; // Variable que indica si se ha encontrado la letra introducida por los usuarios 1 y 2
 
 function validar_1()
 {
     try
     {
-        let letra = document.getElementById("letra_1").value.toUpperCase();
+        let letra = document.getElementById("letra_1").value.toUpperCase(); // Se obtiene la letra introducida por el usuario 1
+        
         if(letra==='')
         {
             return;
         }
 
-        if(resultado_1 === password_1.replace(/./g, "_ "))
+        if(resultado_1 === password_2.replace(/./g, "_ "))
         {
-            resultado_1 = password_1.replace(/./g, "_ ").split(" ");
+            resultado_1 = password_2.replace(/./g, "_ ").split(" "); // Se separa la palabra clave si no ha sido separada antes
         }
 
-        for(let i=0; i<password_1.length; i++)
+        for(let i=0; i<password_2.length; i++) // Se recorre la palabra clave introducida por el usuario 2
         {
-            if(password_1[i] === letra)
+            if(password_2[i] === letra)
             {
-                resultado_1[i] = letra;
-                encontrada = true;
+                resultado_1[i] = letra; // Si se encuentra la letra introducida por el usuario 1, esta letra se introduce en la palabra clave
+                encontrada_1 = true; // Se indica que se ha encontrado una letra
             }
         }
 
-        if(encontrada === true)
+        if(encontrada_1 === true)
         {
             alert("Letra encontrada: " + letra);
-            encontrada === false;
+            encontrada_1 = false;
         }
         else
         {
@@ -52,23 +54,12 @@ function validar_1()
                 case 4: document.getElementById('img_4').style.display = 'none'; document.getElementById('img_3').style.display = 'block'; break;
                 case 5: document.getElementById('img_3').style.display = 'none'; document.getElementById('img_2').style.display = 'block'; break;
                 case 6: document.getElementById('img_2').style.display = 'none'; document.getElementById('img_1').style.display = 'block'; break;
+                case 7: document.getElementById('container_1').style.display = 'none'; document.getElementById('perdedor_1').style.display = 'block'; break;
                 default: break;
             }
-
-            // switch(fallos_1)
-            // {
-            //     case 1: document.getElementById('ahorcado').src = 'img/6.png'; break;
-            //     case 2: document.getElementById('ahorcado').src = 'img/5.png'; break;
-            //     case 3: document.getElementById('ahorcado').src = 'img/4.png'; break;
-            //     case 4: document.getElementById('ahorcado').src = 'img/3.png'; break;
-            //     case 5: document.getElementById('ahorcado').src = 'img/2.png'; break;
-            //     case 6: document.getElementById('ahorcado').src = 'img/1.png'; alert("Usuario 1 pierde"); break;
-            // }
-        }
-        
+        } 
         document.getElementById('result_1').innerHTML = resultado_1;
-
-        if(!resultado_1.includes("_"))
+        if(!resultado_1.includes("_") && resultado_2.includes("_"))
         {
             alert("Usuario 1 gana.");
         }
@@ -76,5 +67,64 @@ function validar_1()
     catch(err)
     {
         alert("ERROR");
+    }
+}
+
+function validar_2()
+{
+    try
+    {
+        let letra = document.getElementById("letra_2").value.toUpperCase();
+        if(letra==='')
+        {
+            return;
+        }
+
+        if(resultado_2 === password_1.replace(/./g, "_ "))
+        {
+            resultado_2 = password_1.replace(/./g, "_ ").split(" ");
+        }
+
+        for(let i=0; i<password_1.length; i++)
+        {
+            if(password_1[i] === letra)
+            {
+                resultado_2[i] = letra;
+                encontrada_2 = true;
+            }
+        }
+
+        if(encontrada_2 === true)
+        {
+            alert("Letra encontrada: " + letra);
+            encontrada_2 = false;
+        }
+        else
+        {
+            alert("Letra no encontrada");
+            fallos_2++;
+
+            switch(fallos_2)
+            {
+                case 1: document.getElementById('img_00').style.display = 'none'; document.getElementById('img_06').style.display = 'block'; break;
+                case 2: document.getElementById('img_06').style.display = 'none'; document.getElementById('img_05').style.display = 'block'; break;
+                case 3: document.getElementById('img_05').style.display = 'none'; document.getElementById('img_04').style.display = 'block'; break;
+                case 4: document.getElementById('img_04').style.display = 'none'; document.getElementById('img_03').style.display = 'block'; break;
+                case 5: document.getElementById('img_03').style.display = 'none'; document.getElementById('img_02').style.display = 'block'; break;
+                case 6: document.getElementById('img_02').style.display = 'none'; document.getElementById('img_01').style.display = 'block'; break;
+                case 7: document.getElementById('container_2').style.display = 'none'; document.getElementById('perdedor_2').style.display = 'block'; break;
+                default: break;
+            }
+        } 
+        document.getElementById('result_2').innerHTML = resultado_2;
+        if(!resultado_2.includes("_") && resultado_1.includes("_"))
+        {
+            alert("Usuario 2 gana.");
+        }
+    }
+    catch(err)
+    {
+        alert("ERROR");
+        console.log(err);
     }
 }
