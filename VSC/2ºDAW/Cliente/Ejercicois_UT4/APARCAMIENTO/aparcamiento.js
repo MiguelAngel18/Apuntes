44f64ff3e64240e93b19973e38d8fb473f8302ef
MiguelAngel18 <migue.angel.c2005@gmail.com>
2024-11-18 22:10:50 +0000

    .

--- VSC/2ºDAW/Cliente/Ejercicois_UT4/APARCAMIENTO/aparcamiento.js
+++ VSC/2ºDAW/Cliente/Ejercicois_UT4/APARCAMIENTO/aparcamiento.js
@@ -1,10 +1,52 @@
-let hoy = document.getElementById("fecha_entrada").value;
-let hora1 = document.getElementById("hora_entrada").value;
-
-function visualizar()
+function precio()
 {
+    let fecha = new Date(); // Fecha actual
+    let hora_actual = new fecha.getHours; // Hora actual
 
-    let fecha2 = document.getElementById("fecha_salida").value;
-    let hora2 = document.getElementById("hora_salida").value;
+
+    let entrada = document.getElementById("entrada").value;
+    entrada = entrada.split("T");
+    
+    let entrada_fecha = entrada[0]; // Fecha de entrada dd/mm/aaaa
+    alert(entrada_fecha)
+    let entrada_hora = entrada[1]; // Hora de entrada hh:mm
+    alert(salida_hora)
+
+    let salida = document.getElementById("salida").value;
+    salida = salida.split(" ");
+    
+    let salida_fecha = entrada[0];
+    let salida_hora = entrada[1];
+
+
+
     let precio = 0.0;
+
+    if(entrada_fecha > salida_fecha || entrada_fecha == null || salida_fecha == null)
+    {
+        return;
+    }
+    if(entrada_hora > salida_hora || entrada_hora == salida_hora || entrada_hora == null || salida_hora == null || entrada_hora === hora_actual)
+    {
+        return;
+    }
+    else if(salida_fecha - entrada_fecha >= 1)
+    {
+        let dias = salida_fecha - entrada_fecha;
+        precio += 20 * dias;
+    }
+    else if(salida_hora - entrada_hora <= 60)
+    {
+        let horas = salida_hora - entrada_hora;
+        if(horas === 1)
+        {
+            precio += 1,20;
+        }
+        else
+        {
+            precio += 1,20;
+            precio += 1,50 * (horas-1);
+        }
+    }
+    document.getElementById(""). precio + "€";
 }
