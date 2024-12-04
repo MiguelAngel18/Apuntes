7699ea495c3253e9f03259bb26d2c533b2a6ca1b
MiguelAngel18 <migue.angel.c2005@gmail.com>
2024-12-04 09:59:18 +0000

    .

--- VSC/2ºDAW/Cliente/Ejercircios_UT5/FORMULARIO/index.js
+++ VSC/2ºDAW/Cliente/Ejercircios_UT5/FORMULARIO/index.js
@@ -4,13 +4,14 @@ let list = [];
 // Creción de los elementos de la lista
 class User 
 {
-    constructor(firstName, lastName, email, gender, origin) 
+    constructor(firstName, lastName, email, gender, origin, interests) 
     {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.gender = gender;
         this.origin = origin;
+        this.interests = interests;
     }
 }
 
@@ -22,21 +23,20 @@ function addData()
     let lastName = document.getElementById('lastName').value;
     let gender = document.querySelector('input[name="info"]:checked')?.value;
     let email = document.getElementById('email').value;
+    let origin = document.getElementById('origin').value;
 
-    let checkboxes = document.querySelectorAll('.checkboxs input[type="checkbox"]:checked');
+    let checkboxes = document.querySelectorAll('div.checkboxs>input[type="checkbox"]:checked.checkbox');
     let interests = Array.from(checkboxes).map((checkbox) => checkbox.value).join(", ") || "Ninguno";
 
-    validacion(firstName, lastName, email, origin);
+    validacion(firstName, lastName, email);
 
-    let user = new User(firstName, lastName, gender, email, origin);
+    let user = new User(firstName, lastName, email, gender, origin, interests);
 
     list.push(user);
     alert("Añadido con éxito");
 
     let dataList = document.getElementById('list'); // Obtener la lista
-
-
-    let option = document.createElement('option'); // Crear un nuevo elemento option
+    let option = document.createElement('option'); // Crear un nuevo elemento option para esa lista
 
     // Configurar los atributos del option
     option.name = 'element';
@@ -46,35 +46,57 @@ function addData()
     // Añadir el nuevo elemento a la lista
     dataList.appendChild(option);
 
-    console.log(`${user.firstName}\n${user.lastName}\n${user.email}\n${user.origin}\n`);
+    console.log(`Nombre: ${user.firstName}\nApellidos: ${user.lastName}\nEmail: ${user.email}\nGénero: ${user.gender}\nOrigen: ${user.origin}\nIntereses: ${user.interests}\n`);
 }
 
 // Visualiza elementos de la lista
-function viewData() 
-{
-    // Seleccionar el botón
+
+    // Asociar la función al evento 'click' del botón
     let viewButton = document.getElementById('viewButton');
-    
-    // Crear una función que se ejecutará cuando se haga clic en el botón
-    function view()
-    {
-        alert('¡Botón!');
+    viewButton.addEventListener('click', viewData);
+
+function viewData()
+{
+    let users = 'Usuarios:\n\n';
+
+    for(let i=0; i<list.length; i++)
+    {    
+        users += `${list[i].firstName} ${list[i].lastName} - ${list[i].email} \n`;
     }
     
-    // Asociar la función al evento 'click' del botón
-    viewButton.addEventListener('click', view);
+    alert(users);
 }
 
 // Elimina elementos de la lista
-function deleteData() 
+function deleteData()
 {
+    let dataList = document.getElementById('list');
+    let selectedOption = dataList.options[dataList.selectedIndex];
 
+    if (selectedOption)
+        {
+        let confirmDelete = confirm(`¿Estás seguro de que deseas eliminar a ${selectedOption.textContent}?`);
+        
+        if (confirmDelete)
+            {
+            // Eliminar del array 'list'
+            let userIndex = Array.from(dataList.options).indexOf(selectedOption);
+            list.splice(userIndex, 1);
+
+            // Eliminar del select
+            dataList.removeChild(selectedOption);
+
+            alert("Eliminado con éxito");
+        }
+    } else
+    {
+        alert("Por favor, selecciona un elemento para eliminar.");
+    }
 }
-
 
 function validacion()
 {
-    if (!lastName || !firstName || !email || !validateEmail(email))
+    if (lastName==='' || firstName==='' || email==='' || validateEmail(email))
     {
         alert("Por favor, completa todos los campos de forma váilda.");
         return;
@@ -85,20 +107,4 @@ function validateEmail(email)
 {
     let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
-}
-
-function validateCheckboxes() {
-    // Seleccionar los checkboxes
-    let checkboxes = document.querySelectorAll('.checkboxs input[type="checkbox"]');
-    
-    // Verificar si al menos uno está seleccionado
-    let isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
-    
-    // Si no hay ninguno seleccionado, mostrar una alerta y evitar el envío del formulario
-    if (!isChecked) {
-        alert('Por favor, seleccione al menos una opción.');
-        return false; // Evitar el envío del formulario
-    }
-    
-    return true; // Permitir el envío del formulario
 }
